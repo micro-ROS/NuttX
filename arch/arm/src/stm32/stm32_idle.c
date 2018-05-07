e75d6d36b9ad531085d3053b88538e81e19107ae
Juan Flores <jfloresm92@gmail.com>
2018-05-07 10:35:43 +0200

    Adding power management support to Olimex-STM32-E407

--- arch/arm/src/stm32/stm32_idle.c
+++ arch/arm/src/stm32/stm32_idle.c
@@ -1,8 +1,9 @@
 /****************************************************************************
- *  arch/arm/src/stm32/stm32_idle.c
+ * configs/stm3210e-eval/src/stm32_idle.c
  *
- *   Copyright (C) 2011-2012, 2015-2016 Gregory Nutt. All rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
+ *   Copyright (C) 2012, 2015-2016 Gregory Nutt. All rights reserved.
+ *   Authors: Gregory Nutt <gnutt@nuttx.org>
+ *            Diego Sanchez <dsanchez@nx-engineering.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -37,24 +38,31 @@
  * Included Files
  ****************************************************************************/
 
-#include <arch/board/board.h>
 #include <nuttx/config.h>
+
 #include <debug.h>
 
 #include <nuttx/arch.h>
-#include <nuttx/board.h>
+#include <nuttx/clock.h>
 #include <nuttx/power/pm.h>
 
-#include <nuttx/irq.h>
+#include <arch/irq.h>
 
-#include "chip.h"
-#include "stm32_pm.h"
+#include <nuttx/board.h>
+#include <arch/board/board.h>
+
 #include "up_internal.h"
+#include "stm32_pm.h"
+#include "stm32_rcc.h"
+#include "stm32_exti.h"
+#include "stm32_rtc.h"
+
+//#include "olimex-stm32-e407.h"
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
-
+/* Configuration ************************************************************/
 /* Does the board support an IDLE LED to indicate that the board is in the
  * IDLE state?
  */
@@ -67,14 +75,169 @@
 #  define END_IDLE()
 #endif
 
+/* Values for the RTC Alarm to wake up from the PM_STANDBY mode
+ * (which corresponds to STM32 stop mode).  If this alarm expires,
+ * the logic in this file will wakeup from PM_STANDBY mode and
+ * transition to PM_SLEEP mode (STM32 standby mode).
+ */
+
+#ifndef CONFIG_PM_ALARM_SEC
+#  define CONFIG_PM_ALARM_SEC 15
+#endif
+
+#ifndef CONFIG_PM_ALARM_NSEC
+#  define CONFIG_PM_ALARM_NSEC 0
+#endif
+
+/* Values for the RTC Alarm to reset from the PM_SLEEP mode (STM32
+ * standby mode).  If CONFIG_PM_SLEEP_WAKEUP is defined in the
+ * configuration, then the logic in this file will program the RTC
+ * alarm to wakeup the processor after an a delay.
+ *
+ * This feature might be useful, for example, in a system that needs to
+ * use minimal power but awake up to perform some task at periodic
+ * intervals.
+ */
+
+#ifdef CONFIG_PM_SLEEP_WAKEUP
+
+#  ifndef CONFIG_RTC_ALARM
+#    error "CONFIG_RTC_ALARM should be enabled to use CONFIG_PM_SLEEP_WAKEUP"
+#  endif
+
+   /* If CONFIG_PM_SLEEP_WAKEUP is defined, then CONFIG_PM_SLEEP_WAKEUP_SEC
+    * and CONFIG_PM_SLEEP_WAKEUP_NSEC define the delay until the STM32
+    * awakens from PM_SLEEP mode.
+    */
+
+#  ifndef CONFIG_PM_SLEEP_WAKEUP_SEC
+#    define CONFIG_PM_SLEEP_WAKEUP_SEC 10
+#  endif
+
+#  ifndef CONFIG_PM_SLEEP_WAKEUP_NSEC
+#    define CONFIG_PM_SLEEP_WAKEUP_NSEC 0
+#  endif
+#endif
+
 #define PM_IDLE_DOMAIN 0 /* Revisit */
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+#if defined(CONFIG_PM) && defined(CONFIG_RTC_ALARM)
+static volatile bool g_alarmwakeup;               /* Wakeup Alarm indicator */
+#endif
 
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
 
 /****************************************************************************
- * Name: up_idlepm
+ * Name: stm32_alarmcb
+ *
+ * Description:
+ *    RTC alarm callback
+ *
+ ****************************************************************************/
+
+#if defined(CONFIG_PM) && defined(CONFIG_RTC_ALARM)
+static void stm32_alarmcb(void)
+{
+  /* Note that we were awaken by an alarm */
+
+  g_alarmwakeup = true;
+}
+#endif
+
+/****************************************************************************
+ * Name: stm32_alarm_exti
+ *
+ * Description:
+ *    RTC alarm EXTI interrupt service routine
+ *
+ ****************************************************************************/
+
+#if defined(CONFIG_PM) && defined(CONFIG_RTC_ALARM)
+static int stm32_alarm_exti(int irq, FAR void *context, FAR void *arg)
+{
+  stm32_alarmcb();
+  return OK;
+}
+#endif
+
+/****************************************************************************
+ * Name: stm32_exti_cancel
+ *
+ * Description:
+ *    Disable the ALARM EXTI interrupt
+ *
+ ****************************************************************************/
+
+#if defined(CONFIG_PM) && defined(CONFIG_RTC_ALARM)
+static void stm32_exti_cancel(void)
+{
+  (void)stm32_exti_alarm(false, false, false, NULL, NULL);
+}
+#endif
+
+/****************************************************************************
+ * Name: stm32_rtc_alarm
+ *
+ * Description:
+ *   Set the alarm
+ *
+ ****************************************************************************/
+
+#if defined(CONFIG_PM) && defined(CONFIG_RTC_ALARM)
+static int stm32_rtc_alarm(time_t tv_sec, time_t tv_nsec, bool exti)
+{
+  struct timespec alarmtime;
+  int ret;
+
+  /* Configure to receive RTC Alarm EXTI interrupt */
+
+  if (exti)
+    {
+      /* TODO: Make sure that that is no pending EXTI interrupt */
+
+      (void)stm32_exti_alarm(true, true, true, stm32_alarm_exti, NULL);
+    }
+
+  /* Configure the RTC alarm to Auto Wake the system */
+
+  (void)up_rtc_gettime(&alarmtime);
+
+  alarmtime.tv_sec  += tv_sec;
+  alarmtime.tv_nsec += tv_nsec;
+
+  /* The tv_nsec value must not exceed 1,000,000,000. That
+   * would be an invalid time.
+   */
+
+  if (alarmtime.tv_nsec >= NSEC_PER_SEC)
+    {
+      /* Carry to the seconds */
+
+      alarmtime.tv_sec++;
+      alarmtime.tv_nsec -= NSEC_PER_SEC;
+    }
+
+  /* Set the alarm */
+
+  g_alarmwakeup = false;
+  ret = stm32_rtc_setalarm(&alarmtime, stm32_alarmcb);
+  if (ret < 0)
+    {
+      serr("ERROR: Warning: The alarm is already set\n");
+    }
+
+  return ret;
+}
+#endif
+
+/****************************************************************************
+ * Name: stm32_idlepm
  *
  * Description:
  *   Perform IDLE state power management.
@@ -82,28 +245,56 @@
  ****************************************************************************/
 
 #ifdef CONFIG_PM
-static void up_idlepm(void)
+static void stm32_idlepm(void)
 {
   static enum pm_state_e oldstate = PM_NORMAL;
   enum pm_state_e newstate;
-  irqstate_t flags;
   int ret;
 
-  /* Decide, which power saving level can be obtained */
+  /* The following is logic that is done after the wake-up from PM_STANDBY
+   * state.  It decides whether to go back to the PM_NORMAL or to the deeper
+   * power-saving mode PM_SLEEP:  If the alarm expired with no "normal"
+   * wake-up event, then PM_SLEEP is entered.
+   *
+   * Logically, this code belongs at the end of the PM_STANDBY case below,
+   * does not work in the position for some unkown reason.
+   */
 
-  newstate = pm_checkstate(PM_IDLE_DOMAIN);
+  if (oldstate == PM_STANDBY)
+    {
+      /* Were we awakened by the alarm? */
+
+#ifdef CONFIG_RTC_ALARM
+      if (g_alarmwakeup)
+        {
+          /* Yes.. Go to SLEEP mode */
+
+          newstate = PM_SLEEP;
+        }
+      else
+#endif
+        {
+          /* Resume normal operation */
+
+          newstate = PM_NORMAL;
+        }
+    }
+  else
+    {
+      /* Let the PM system decide, which power saving level can be obtained */
+
+      newstate = pm_checkstate(PM_IDLE_DOMAIN);
+    }
 
   /* Check for state changes */
 
   if (newstate != oldstate)
     {
-      flags = enter_critical_section();
-
-      /* Perform board-specific, state-dependent logic here */
-
       _info("newstate= %d oldstate=%d\n", newstate, oldstate);
 
-      /* Then force the global state change */
+      sched_lock();
+
+      /* Force the global state change */
 
       ret = pm_changestate(PM_IDLE_DOMAIN, newstate);
       if (ret < 0)
@@ -111,42 +302,115 @@ static void up_idlepm(void)
           /* The new state change failed, revert to the preceding state */
 
           (void)pm_changestate(PM_IDLE_DOMAIN, oldstate);
-        }
-      else
-        {
-          /* Save the new state */
 
-          oldstate = newstate;
+          /* No state change... */
+
+          goto errout;
         }
 
-      /* MCU-specific power management logic */
+      /* Then perform board-specific, state-dependent logic here */
 
       switch (newstate)
         {
         case PM_NORMAL:
+          {
+            /* If we just awakened from PM_STANDBY mode, then reconfigure
+             * clocking.
+             */
+
+            if (oldstate == PM_STANDBY)
+              {
+                /* Re-enable clocking */
+
+                stm32_clockenable();
+
+                /* The system timer was disabled while in PM_STANDBY or
+                 * PM_SLEEP modes.  But the RTC has still be running:  Reset
+                 * the system time the current RTC time.
+                 */
+
+#ifdef CONFIG_RTC
+                clock_synchronize();
+#endif
+              }
+          }
           break;
 
         case PM_IDLE:
+          {
+          }
           break;
 
         case PM_STANDBY:
-          stm32_pmstop(true);
+          {
+            /* Set the alarm as an EXTI Line */
+#ifdef CONFIG_RTC_ALARM
+            stm32_rtc_alarm(CONFIG_PM_ALARM_SEC, CONFIG_PM_ALARM_NSEC, true);
+#endif
+            /* Wait 10ms */
+
+            up_mdelay(10);
+
+            /* Enter the STM32 stop mode */
+
+            (void)stm32_pmstop(true);
+
+            /* We have been re-awakened by some even:  A button press?
+             * An alarm?  Cancel any pending alarm and resume the normal
+             * operation.
+             */
+
+#ifdef CONFIG_RTC_ALARM
+            stm32_exti_cancel();
+            ret = stm32_rtc_cancelalarm();
+            if (ret < 0)
+              {
+                swarn("WARNING: Cancel alarm failed\n");
+              }
+#endif
+            /* Note:  See the additional PM_STANDBY related logic at the
+             * beginning of this function.  That logic is executed after
+             * this point.
+             */
+          }
           break;
 
         case PM_SLEEP:
-          (void)stm32_pmstandby();
+          {
+            /* We should not return from standby mode.  The only way out
+             * of standby is via the reset path.
+             */
+
+            /* Configure the RTC alarm to Auto Reset the system */
+
+#ifdef CONFIG_PM_SLEEP_WAKEUP
+            stm32_rtc_alarm(CONFIG_PM_SLEEP_WAKEUP_SEC, CONFIG_PM_SLEEP_WAKEUP_NSEC, false);
+#endif
+            /* Wait 10ms */
+
+            up_mdelay(10);
+
+            /* Enter the STM32 standby mode */
+
+            (void)stm32_pmstandby();
+          }
           break;
 
         default:
           break;
         }
 
-      leave_critical_section(flags);
+      /* Save the new state */
+
+      oldstate = newstate;
+
+errout:
+      sched_unlock();
     }
 }
 #else
-#  define up_idlepm()
-#endif
+#  define stm32_idlepm()
+#endif /* CONFIG_PM */
 
 /****************************************************************************
  * Public Functions
@@ -177,36 +441,8 @@ void up_idle(void)
 
   /* Perform IDLE mode power management */
 
-  up_idlepm();
-
-  /* Sleep until an interrupt occurs to save power.
-   *
-   * NOTE:  There is an STM32F107 errata that is fixed by the following
-   * workaround:
-   *
-   * "2.17.11 Ethernet DMA not working after WFI/WFE instruction
-   *  Description
-   *  If a WFI/WFE instruction is executed to put the system in sleep mode
-   *    while the Ethernet MAC master clock on the AHB bus matrix is ON and all
-   *    remaining masters clocks are OFF, the Ethernet DMA will be not able to
-   *    perform any AHB master accesses during sleep mode."
-   *
-   *  Workaround
-   *    Enable DMA1 or DMA2 clocks in the RCC_AHBENR register before
-   *    executing the WFI/WFE instruction."
-   *
-   * Here the workaround is just to avoid SLEEP mode for the connectivity
-   * line parts if Ethernet is enabled.  The errate recommends a  more
-   * general solution:  Enabling DMA1/2 clocking in stm32f10xx_rcc.c if the
-   * STM32107 Ethernet peripheral is enabled.
-   */
-
-#if !defined(CONFIG_STM32_CONNECTIVITYLINE) || !defined(CONFIG_STM32_ETHMAC)
-#if !(defined(CONFIG_DEBUG_SYMBOLS) && defined(CONFIG_STM32_DISABLE_IDLE_SLEEP_DURING_DEBUG))
   BEGIN_IDLE();
-  asm("WFI");
+  stm32_idlepm();
   END_IDLE();
-#endif
-#endif
 #endif
 }
