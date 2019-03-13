2d19e750ebbb2833a274ba1fca649ab91e87804c
Luetkebohle Ingo (CR/AEX3) <Ingo.Luetkebohle@de.bosch.com>
2019-03-13 10:51:59 +0100

    start of drive_base configuration

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $1 = "olimex-stm32-e407" ]; then
-  openocd -f interface/ftdi/olimex-arm-usb-tiny-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase nuttx.bin 0x08000000"
+  openocd -f interface/ftdi/olimex-arm-usb-ocd-h.cfg -f target/stm32f4x.cfg -c init -c "reset halt" -c "flash write_image erase nuttx.bin 0x08000000"
 elif [ $1 = "stm32l1" ]; then
   openocd -f interface/stlink-v2.cfg -f target/stm32l1.cfg -c init -c "reset halt" -c "flash write_image erase nuttx.bin 0x08000000"
 else
