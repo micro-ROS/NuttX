650776a50d58a31286a2f2f3b05ecb9ead931a75
Alex M <amalki@piap.pl>
2020-08-20 11:12:46 +0200

    ctf: tracing element: fixing the stack

--- tracing/tracing_elements/tracing_stack.c
+++ tracing/tracing_elements/tracing_stack.c
@@ -18,6 +18,9 @@ extern atomic_val_t atomic_get
 extern void *memcpy(FAR void *, FAR const void *, size_t) __attribute__((no_instrument_function));
 
 struct tracing_stack_func {
+#ifdef CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
+	uint32_t reached_max;
+#endif
 	bool is_tracing;
 } tracing_ctx [64];
 
@@ -28,105 +31,55 @@ void __cyg_profile_func_enter(void *this_fn, void *call_site)
 		return;
 	}
 
-#if 0
-	asm volatile (
-			"   mrs r2, ipsr        \n" /* Check whether we are in interrupt mode */
-			"   cmp r2, #0          \n" /* since we don't switch r10 on interrupt entry, we */
-			"   bne 1f              \n" /* can't detect overflow of the interrupt stack. */
-			"1:                     \n"
-			"   bx lr               \n"
-		     );
-#endif
-
-
+	if (is_tracing_thread()) {
+		return;
+	}
 
 	struct tcb_s *thread = sched_self();
 	if (!thread || thread == 0xffffffff)
-		pid = 63;
-	else  {
-		if (pid > 62) {
-			return;
-		}
-		pid = thread->pid;
+		return;
+
+	pid = thread->pid;
+	if (!pid || pid > 63) {
+		return;
 	}
+
+	if (thread->task_state != TSTATE_TASK_RUNNING) {
+		return;
+	}
+
 
 	if (tracing_ctx[pid].is_tracing) {
 		return;
 	}
 
 #ifdef CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
-	static uint32_t reached_max = 0;
 	static uint32_t call_count = 0;
 	register void *sp asm ("sp");
 	uint32_t sp_ptr = (uint32_t)(uintptr_t) sp;
-
 	sp_ptr = ((uint32_t) (uintptr_t)thread->adj_stack_ptr) - sp_ptr;
-
+	if (sp_ptr > (thread->adj_stack_size << 3)) {
+		// Probably the scheduler doing some work withou updating the 
+		// pid 
+		return;		
+	}
 #if 0
 	if  (!(++calls_cnt % CONFIG_TRACE_STACK_USAGE_CALLS_CNT_SAMPLING)) {
 		sys_trace_stack_usage(thread, this_fn, sp_ptr);
 	} 
 #endif
-	if (reached_max < sp_ptr) {
+	if (tracing_ctx[pid].reached_max < sp_ptr) {
 		tracing_ctx[pid].is_tracing = true;
 		sys_trace_memory_static_alloc(this_fn, sp_ptr);
-		reached_max = sp_ptr;
+		tracing_ctx[pid].reached_max = sp_ptr;
 		tracing_ctx[pid].is_tracing = false;
 	}
 #endif // CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
-
-#if 0
-
-
-
-	for (int i = (int) pid_backtrace[pid].count; i > -1 ; i--) {
-	 	if (&sys_trace_memory_static_alloc == pid_backtrace[i].address[i]) {
-			irq_unlock(key);
-			return ;
-		}
-
-	 	if (&__cyg_profile_func_exit == pid_backtrace[i].address[i]) {
-			irq_unlock(key);
-			return ;
-		}
-	}
-
-	pid_backtrace[pid].address[pid_backtrace[pid].count] = call_site;
-	pid_backtrace[pid].count++;
-	register void *sp asm ("sp");
-	static uint32_t maxusage = 0;
-	uint32_t sp_ptr = (uint32_t)(uintptr_t) sp;
-
-	sp_ptr = ((uint32_t) (uintptr_t)thread->adj_stack_ptr) - sp_ptr;
-	if (maxusage < sp_ptr) {
-		sys_trace_memory_static_alloc(this_fn, sp_ptr);
-		maxusage = sp_ptr;
-	}
-#if 0
-	if (max_size < sp_ptr 
-	    || !(++calls_cnt % CONFIG_TRACE_STACK_USAGE_CALLS_CNT_SAMPLING)) {
-		sys_trace_stack_usage(thread, this_fn, sp_ptr);
-	}
-#endif
-#ifdef CONFIG_TRACE_CTF_FUNCTIONS_USAGE
-	 sys_trace_func_usage_enter(this_fn);
-#endif // CONFIG_TRACE_CTF_FUNCTIONS_USAGE
-
-	irq_unlock(key);
-#endif
 }
 
 void __cyg_profile_func_exit (void *this_fn,
 	       	void *call_site)
 {
-	asm volatile (
-			"   mrs r2, ipsr        \n" /* Check whether we are in interrupt mode */
-			"   cmp r2, #0          \n" /* since we don't switch r10 on interrupt entry, we */
-			"   bne 1f              \n"
-			"1:                     \n"
-			"   bx lr               \n"
-		     );
-
 #ifdef CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
 #endif //CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
 #ifdef CONFIG_TRACE_CTF_FUNCTIONS_USAGE
