7e0452bb520605c7f91b4a98848e5cad3de1ae3d
Alex M <amalki@piap.pl>
2020-08-20 14:08:18 +0200

    ctf: tracing: static memory usage: fix

--- tracing/tracing_elements/tracing_stack.c
+++ tracing/tracing_elements/tracing_stack.c
@@ -18,11 +18,15 @@ extern atomic_val_t atomic_get
 extern void *memcpy(FAR void *, FAR const void *, size_t) __attribute__((no_instrument_function));
 
 struct tracing_stack_func {
+	bool is_tracing_enter;
+#ifdef CONFIG_TRACE_CTF_FUNCTIONS_USAGE
+	bool is_tracing_exit;
+#endif
 #ifdef CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
 	uint32_t reached_max;
 #endif
-	bool is_tracing;
 } tracing_ctx [64];
+
 
 void __cyg_profile_func_enter(void *this_fn, void *call_site)
 {
@@ -49,10 +53,11 @@ void __cyg_profile_func_enter(void *this_fn, void *call_site)
 	}
 
 
-	if (tracing_ctx[pid].is_tracing) {
+	if (tracing_ctx[pid].is_tracing_enter) {
 		return;
 	}
 
+	tracing_ctx[pid].is_tracing_enter = true;
 #ifdef CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
 #if CONFIG_TRACE_STACK_USAGE_CALLS_CNT_SAMPLING > 0
 	static uint32_t call_count = 0;
@@ -64,10 +69,10 @@ void __cyg_profile_func_enter(void *this_fn, void *call_site)
 	if (sp_ptr > (thread->adj_stack_size << 4)) {
 		// Probably the scheduler doing some work withou updating the 
 		// pid 
+		tracing_ctx[pid].is_tracing_enter = false;
 		return;		
 	}
 
-	tracing_ctx[pid].is_tracing = true;
 #if CONFIG_TRACE_STACK_USAGE_CALLS_CNT_SAMPLING > 0
 	if  (!(++call_count % CONFIG_TRACE_STACK_USAGE_CALLS_CNT_SAMPLING)) {
 		sys_trace_memory_static_alloc(this_fn, sp_ptr);
@@ -77,16 +82,46 @@ void __cyg_profile_func_enter(void *this_fn, void *call_site)
 		sys_trace_memory_static_alloc(this_fn, sp_ptr);
 		tracing_ctx[pid].reached_max = sp_ptr;
 	}
-	tracing_ctx[pid].is_tracing = false;
 #endif // CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
+#ifdef CONFIG_TRACE_CTF_FUNCTIONS_USAGE
+	sys_trace_func_usage_exit(this_fn);
+#endif // CONFIG_TRACE_CTF_FUNCTIONS_USAGE
+	tracing_ctx[pid].is_tracing_enter = false;
 }
 
 void __cyg_profile_func_exit (void *this_fn,
 	       	void *call_site)
 {
-#ifdef CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
-#endif //CONFIG_TRACE_CTF_MEMORY_STATIC_INFO
 #ifdef CONFIG_TRACE_CTF_FUNCTIONS_USAGE
+	uint32_t pid;
+	if (!is_tracing_enabled()) {
+		return;
+	}
+
+	if (is_tracing_thread()) {
+		return;
+	}
+
+	struct tcb_s *thread = sched_self();
+	if (!thread || thread == 0xffffffff)
+		return;
+
+	pid = thread->pid;
+	if (!pid || pid > 63) {
+		return;
+	}
+
+	if (thread->task_state != TSTATE_TASK_RUNNING) {
+		return;
+	}
+
+
+	if (tracing_ctx[pid].is_tracing_exit) {
+		return;
+	}
+
+	tracing_ctx[pid].is_tracing_exit = true;
 	sys_trace_func_usage_exit(this_fn);
+	tracing_ctx[pid].is_tracing_exit = false;
 #endif
 }
