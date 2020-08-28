f162e22bf2bfc881b839729d945255cdca6e5b20
Alex M <amalki@piap.pl>
2020-08-28 11:27:01 +0000

    provide ways to determining the amount of biased amount of time

--- tracing/tracing_core.c
+++ tracing/tracing_core.c
@@ -15,6 +15,10 @@
 #include <nuttx/kthread.h>
 #include <nuttx/tracing/tracing_common.h>
 #include <nuttx/arch.h>
+#ifdef CONFIG_ASYNC_CTF_TRACING
+#include <nuttx/tracing/ctf_map.h>
+#include <nuttx/tracing/ctf_types.h>
+#endif
 
 #define TRACING_CMD_ENABLE  "enable"
 #define TRACING_CMD_DISABLE "disable"
@@ -46,23 +50,94 @@ static atomic_t tracing_packet_drop_num;
 static struct tracing_backend *working_backend;
 
 #ifdef CONFIG_ASYNC_CTF_TRACING
+
+/*
+ * Obtain a field's size at compile-time.
+ */
+#define CTF_INTERNAL_FIELD_SIZE(x)      + sizeof(x)
+
+
+#define CTF_INTERNAL_FIELD_APPEND(x)			 \
+	{						 \
+		memcpy(epacket_cursor, &(x), sizeof(x)); \
+		epacket_cursor += sizeof(x);		 \
+	}
+
+/*
+ * Gather fields to a contiguous event-packet, then atomically emit.
+ */
+#define CTF_GATHER_FIELDS(...)						    \
+{									    \
+	u8_t epacket[0 MAP(CTF_INTERNAL_FIELD_SIZE, ##__VA_ARGS__)];	    \
+	u8_t *epacket_cursor = &epacket[0];				    \
+									    \
+	MAP(CTF_INTERNAL_FIELD_APPEND, ##__VA_ARGS__)			    \
+	tracing_buffer_handle(epacket, sizeof(epacket));                    \
+}
+
+#define CTF_EVENT(...)							    \
+	{								    \
+		const u64_t tstamp = tracing_get_counter_for_dump();	    \
+		CTF_GATHER_FIELDS(tstamp, __VA_ARGS__)			    \
+	}
+
+/* Anonymous compound literal with 1 member. Legal since C99.
+ * This permits us to take the address of literals, like so:
+ *  &CTF_LITERAL(int, 1234)
+ *
+ * This may be required if a ctf_bottom layer uses memcpy.
+ *
+ * NOTE string literals already support address-of and sizeof,
+ * so string literals should not be wrapped with CTF_LITERAL.
+ */
+#define CTF_LITERAL(type, value)  ((type) { (type)(value) })
+
+
 static pid_t tracing_task_pid;
 static struct work_s tracing_trigger_flush;
 static sem_t tracing_thread_sem;
+volatile uint64_t g_measuredtimestamp = 0;
+
+uint64_t tracing_get_counter_value(void)
+{
+#ifdef CONFIG_TRACE_TIMESTAMP_CUSTOM_BOARD_TIMER
+	struct timespec time;
+
+	if (!is_tracing_enabled()) {
+		return 0;
+	}
+
+	stm32_freerun_counter(&g_freerun, &time);
+	return time.tv_nsec + (uint64_t)time.tv_sec * NSEC_PER_SEC;
+#elif TRACE_TIMESTAMP_REFERENCE_SYSTICK_TIMER
+	return g_system_timer;
+#else
+#error "No Counter selected"
+#endif //TRACE_TIMESTAMP_CUSTOM_BOARD_TIMER
+}
+
+uint64_t tracing_get_counter_for_dump(void)
+{
+	return g_measuredtimestamp;
+}
 
 static int tracing_thread_func(int argc, char *argv[])
 {
 	u8_t *transferring_buf;
+	u8_t start_id = 0xF0;
+	u8_t stop_id = 0xF1;
 	u32_t transferring_length, tracing_buffer_max_length;
 
 	//tracing_task_pid = sched_self()->pid;
 
 	tracing_buffer_max_length = tracing_buffer_capacity_get();
-
 	while (true) {
 		if (tracing_buffer_is_empty()) {
+			g_measuredtimestamp =  tracing_get_counter_value();
+			CTF_EVENT(CTF_LITERAL(u8_t, stop_id));
 			nxsem_wait(&tracing_thread_sem);
 		} else {
+			g_measuredtimestamp =  tracing_get_counter_value();
 			transferring_length =
 				tracing_buffer_get_claim(
 						&transferring_buf,
@@ -70,6 +145,7 @@ static int tracing_thread_func(int argc, char *argv[])
 			tracing_buffer_handle(transferring_buf,
 					      transferring_length);
 			tracing_buffer_get_finish(transferring_length);
+			CTF_EVENT(CTF_LITERAL(u8_t, start_id));
 		}
 	}
 
@@ -103,24 +179,6 @@ static int32_t tracing_init_timer(void)
 	/* Print the timer status */
 
 #endif //CONFIG_TRACE_TIMESTAMP_CUSTOM_BOARD_TIMER
-}
-
-uint64_t tracing_get_counter_value(void)
-{
-#ifdef CONFIG_TRACE_TIMESTAMP_CUSTOM_BOARD_TIMER
-	struct timespec time;
-
-	if (!is_tracing_enabled() || is_tracing_thread()) {
-		return 0;
-	}
-
-	stm32_freerun_counter(&g_freerun, &time);
-	return time.tv_nsec + (uint64_t)time.tv_sec * NSEC_PER_SEC;
-#elif TRACE_TIMESTAMP_REFERENCE_SYSTICK_TIMER
-	return g_system_timer;
-#else
-#error "No Counter selected"
-#endif //TRACE_TIMESTAMP_CUSTOM_BOARD_TIMER
 }
 
 int tracing_init(void)
@@ -164,7 +222,7 @@ void tracing_trigger_output(bool before_put_is_empty)
 			   &tracing_trigger_flush,
 			   tracing_thread_timer_expiry_fn,
 			   NULL,
-			   10);
+			   0);
 	}
 }
 
